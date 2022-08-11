672346933d22708d1f0769fad4bb0a8552a59697
Jesus Reyes <luisdejesus200122@gmail.com>
2022-08-11 13:59:02 -0600

    Google SignIn Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar_jwt");
+const { gooleVerify, googleVerify } = require("../helpers/google.verify");
 
 
 const login = async (req, res = response) => {
@@ -53,7 +54,49 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+        let usuario = await Usuario.findOne({ correo });
+
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                rol: 'USER_ROLE',
+                password: 'Autenticado con Google',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+        if (!usuario.estado) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'Hable con el administrador, usuario bloqueado',
+            });
+        }
+        const token = await generarJWT(usuario.id);
+        res.json({
+            ok: true,
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no valido',
+        });
+
+    }
+
+}
+
 
 module.exports = {
     login,
+    googleSignIn
 }
